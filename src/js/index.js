b7e3d9d29665b2dbcf320c793df51ef614c27925
abdulbasithb <zithron70@gmail.com>
2018-01-17 17:58:57 +0700

    add kategori lomba joints

---
+++ src/js/index.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    // buka tutup navbar hamburger
+    $(".navbar-burger").click(function () {
+        $("#navbarMenuHeroC").toggleClass("is-active");
+        $(".navbar-burger > span").toggleClass("is-active");
+    });
+
+    // mengganti login button menjadi warna pink
+    $("#loginButton").addClass("is-pink");
+
+    var prev = 53;
+    // console.log(prev + " prev awal");
+
+    $(window).scroll(function () {
+        // nilai scroll
+        var wScroll = $(window).scrollTop();
+        // console.log(wScroll + " wScroll");
+        // console.log(prev + " prev akhir");
+
+        // conditional navbar fixed dan logo joints
+        if (wScroll > 53) {
+            // navbar
+            $(".navbar").addClass("is-fixed-top");
+            // login button
+            $("#loginButton").removeClass("is-inverted");
+            // logo joints
+            $("#logoJoints").attr("src", "images/logo-pink.png");
+        } else {
+            // navbar
+            $(".navbar").removeClass("is-fixed-top");
+            // login button
+            $("#loginButton").addClass("is-inverted");
+            // logo joints
+            $("#logoJoints").attr("src", "images/index/logo.png");
+        }
+
+        // conditional navbar muncul saat di scroll keatas
+        $(".navbar").toggleClass("hiddenUp", wScroll > prev);
+        prev = wScroll;
+
+    });
+});
---
+++ src/js/kompetisi.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    // buka tutup navbar hamburger
+    $(".navbar-burger").click(function () {
+        $("#navbarMenuHeroC").toggleClass("is-active");
+        $(".navbar-burger > span").toggleClass("is-active");
+    });
+
+    // mengganti login button menjadi warna hijau
+    $("#loginButton").addClass("is-success");
+
+    var prev = 53;
+    // console.log(prev + " prev awal");
+
+    $(window).scroll(function () {
+        // nilai scroll
+        var wScroll = $(window).scrollTop();
+        // console.log(wScroll + " wScroll");
+        // console.log(prev + " prev akhir");
+
+        // conditional navbar fixed dan logo joints
+        if (wScroll > 53) {
+            // navbar
+            $(".navbar").addClass("is-fixed-top");
+            // mennganti menjadi warna hijau
+            $(".navbar-item").addClass("has-text-success-reverse");
+            // login button
+            $("#loginButton").removeClass("is-inverted");
+            // logo joints
+            $("#logoJoints").attr("src", "images/logo-hijau.png");
+        } else {
+            // navbar
+            $(".navbar").removeClass("is-fixed-top");
+            // login button
+            $("#loginButton").addClass("is-inverted");
+            // logo joints
+            $("#logoJoints").attr("src", "images/index/logo.png");
+        }
+
+        // conditional navbar muncul saat di scroll keatas
+        $(".navbar").toggleClass("hiddenUp", wScroll > prev);
+        prev = wScroll;
+
+    });
+});

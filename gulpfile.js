723408413b12fbe2c2a8fba66f30b49180354484
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2018-01-17 14:29:54 +0700

    ccomp page and kompetisi.html fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
 
 // Compile Sass & Inject Into Browser
-gulp.task('sass', function() {
-    return gulp.src(['./src/scss/style.scss', 'src/scss/admin.scss', 'src/scss/dashboard.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+gulp.task("sass", function() {
+  return gulp
+    .src([
+      "./src/scss/style.scss",
+      "src/scss/admin.scss",
+      "src/scss/dashboard.scss"
+    ])
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
 });
 
 // Watch Sass & Serve
-gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-        server: "./src"  
-    });
+gulp.task("serve", ["sass"], function() {
+  browserSync.init({
+    server: "./src"
+  });
 
-    gulp.watch(['src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+  gulp.watch(["src/scss/*.scss"], ["sass"]);
+  gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 // Default Task
-gulp.task('default', ['serve']);
+gulp.task("default", ["serve"]);

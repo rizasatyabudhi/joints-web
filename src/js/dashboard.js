910e4b74b87c21ccfd23bedd529eb4057047327e
abdulbasithb <zithron70@gmail.com>
2018-01-23 14:12:01 +0700

    add admin datatim dan pembayaran

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -6,36 +6,7 @@ $(document).ready(function () {
         $(".navbar-item").addClass("has-text-success-reverse");      
     });
 
-
-    // var prev = 53;
-    // // console.log(prev + " prev awal");
-
-    // $(window).scroll(function () {
-    //     // nilai scroll
-    //     var wScroll = $(window).scrollTop();
-    //     // console.log(wScroll + " wScroll");
-    //     // console.log(prev + " prev akhir");
-
-    //     // conditional navbar fixed dan logo joints
-    //     if (wScroll > 53) {
-    //         // navbar
-    //         $(".navbar").addClass("is-fixed-top");
-    //         // login button
-    //         $("#loginButton").removeClass("is-inverted");
-    //         // logo joints
-    //         $("#logoJoints").attr("src", "images/logo-pink.png");
-    //     } else {
-    //         // navbar
-    //         $(".navbar").removeClass("is-fixed-top");
-    //         // login button
-    //         $("#loginButton").addClass("is-inverted");
-    //         // logo joints
-    //         $("#logoJoints").attr("src", "images/index/logo.png");
-    //     }
-
-    //     // conditional navbar muncul saat di scroll keatas
-    //     $(".navbar").toggleClass("hiddenUp", wScroll > prev);
-    //     prev = wScroll;
-
-    // });
+    $("#hamburgerMenu").click(function () {
+        $("#sideNav").toggleClass("is-toggle");
+    });
 });

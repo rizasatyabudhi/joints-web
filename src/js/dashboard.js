842af9c3bbe9c5e9e0e0a3ce75095ecda2afb3d6
abdulbasithb <zithron70@gmail.com>
2018-03-14 23:34:50 +0700

    add invoicce-pembayaran.html & data-tim-perusahaan.html

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -3,10 +3,20 @@ $(document).ready(function () {
     $(".navbar-burger").click(function () {
         $("#navbarMenuHeroC").toggleClass("is-active");
         $(".navbar-burger > span").toggleClass("is-active");
-        $(".navbar-item").addClass("has-text-success-reverse");      
+        $(".navbar-item").addClass("has-text-success-reverse");
     });
 
     $("#hamburgerMenu").click(function () {
         $("#sideNav").toggleClass("is-toggle");
+    });
+
+    $('#tabs li').on('click', function () {
+        var tab = $(this).data('tab');
+
+        $('#tabs li').removeClass('is-active');
+        $(this).addClass('is-active');
+
+        $('#tab-content .column').removeClass('is-active');
+        $('.column[data-content="' + tab + '"]').addClass('is-active');
     });
 });
